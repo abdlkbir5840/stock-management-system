8b76446a12bb84f25ef692b87834e0dbd7087d46
unknown <ysfmh2002@gmail.com>
2023-12-20 17:13:26 +0100

    new version

--- routes/api.php
+++ routes/api.php
@@ -76,26 +76,6 @@ Route::post('registerClient', [AuthController::class, 'registerClient']);
 
 
 Route::group(['middleware'=> ['auth:sanctum']], function () {
-    Route::get('commandes', [CommandeController::class, 'index']);
-    Route::delete('commandes/{id}', [CommandeController::class, 'destroy']);
-    Route::put('commandes/{id}', [CommandeController::class, 'update']);
-    Route::get('commandes/{id}', [CommandeController::class, 'show']);
-    Route::post('commandes', [CommandeController::class, 'store']);
-
-    Route::get('clients', [ClientController::class, 'index']);
-    Route::post('clients', [ClientController::class, 'store']);
-    Route::get('clients/{column1}/{param1}', [ClientController::class, 'show']);
-    Route::get('clients/{column1}/{param1}/{column2}/{param2}', [ClientController::class, 'show_two']);
-    Route::delete('clients/{id}', [ClientController::class, 'destroy']);
-    Route::put('clients/{id}', [ClientController::class, 'update']);
-
-    Route::get('produits', [PrduitController::class, 'index']);
-    Route::get('produits/all', [PrduitController::class, 'getAllProducts']);
-    Route::post('produits', [PrduitController::class, 'store']);
-    Route::post('produits/quantite/{produitId}/{fournisseurId}', [PrduitController::class, 'addQuantite']);
-    Route::get('produits/{column}/{param}', [PrduitController::class, 'show']);
-    Route::put('produits/{id}', [PrduitController::class, 'update']);
-    Route::delete('produits/{id}', [PrduitController::class, 'destroy']);
 
     Route::get('fournisseurs', [FournisseurController::class, 'index']);
     Route::get('fournisseurs/all', [FournisseurController::class, 'getAll']);
@@ -103,47 +83,76 @@ Route::group(['middleware'=> ['auth:sanctum']], function () {
     Route::get('fournisseurs/{column}/{param}', [FournisseurController::class, 'show']);
     Route::put('fournisseurs/{id}', [FournisseurController::class, 'update']);
     Route::delete('fournisseurs/{id}', [FournisseurController::class, 'destroy']);
+});
+Route::group(['middleware'=> ['auth:sanctum']], function () {
+    Route::get('commandes', [CommandeController::class, 'index']);
+    Route::delete('commandes/{id}', [CommandeController::class, 'destroy']);
+    Route::put('commandes/{id}', [CommandeController::class, 'update']);
+    Route::get('commandes/{id}', [CommandeController::class, 'show']);
+    Route::post('commandes', [CommandeController::class, 'store']);
+});
 
-    Route::get('packProduits', [Produit_PackController::class, 'index']);
-    Route::post('packProduits', [Produit_PackController::class, 'store']);
-    Route::get('packProduits/{column}/{param}', [Produit_PackController::class, 'show']);
-    Route::delete('packProduits/{pack_id}/{produit_id}', [Produit_PackController::class, 'destroy']);
-    Route::put('packProduits/{id}', [Produit_PackController::class, 'update']);
-    Route::get('factures', [FactureController::class, 'index']);
-    Route::post('factures', [FactureController::class, 'store']);
-    Route::get('factures/{column}/{param}', [FactureController::class, 'show']);
-    Route::delete('factures/{id}', [FactureController::class, 'destroy']);
-    Route::put('factures/{id}', [FactureController::class, 'update']);
-
-    Route::get('packs', [PackController::class, 'index']);
-    Route::post('packs', [PackController::class, 'store']);
-    Route::get('packs/{column}/{param}', [PackController::class, 'show']);
-    Route::delete('packs/{id}', [PackController::class, 'destroy']);
-    Route::put('packs/{id}', [PackController::class, 'update']);
-
-    Route::get('orderStatuses', [OrderStatusController::class, 'index']);
-    Route::get('orderStatuses/{id}', [OrderStatusController::class, 'show']);
-    Route::post('orderStatuses', [OrderStatusController::class, 'store']);
-    Route::put('orderStatuses/{id}', [OrderStatusController::class, 'update']);
-    Route::delete('orderStatuses/{id}', [OrderStatusController::class, 'destroy']);
-
-    Route::get('discounts', [DiscountController::class, 'index']);
-    Route::get('discounts/{id}', [DiscountController::class, 'show']);
-    Route::post('discounts', [DiscountController::class, 'store']);
-    Route::put('discounts/{id}', [DiscountController::class, 'update']);
-    Route::delete('discounts/{id}', [DiscountController::class, 'destroy']);
-
-
+Route::group(['middleware'=> ['auth:sanctum']], function () {
+    Route::get('clients', [ClientController::class, 'index']);
+    Route::post('clients', [ClientController::class, 'store']);
+    Route::get('clients/{column1}/{param1}', [ClientController::class, 'show']);
+    Route::get('clients/{column1}/{param1}/{column2}/{param2}', [ClientController::class, 'show_two']);
+    Route::delete('clients/{id}', [ClientController::class, 'destroy']);
+    Route::put('clients/{id}', [ClientController::class, 'update']);
+});
+Route::group(['middleware'=> ['auth:sanctum']], function () {
     Route::get('categories', [CategorieController::class, 'index']);
     Route::get('categories/all', [CategorieController::class, 'getAll']);
     Route::post('categories', [CategorieController::class, 'store']);
     Route::get('categories/{column}/{param}', [CategorieController::class, 'show']);
     Route::put('categories/{id}', [CategorieController::class, 'update']);
     Route::delete('categories/{id}', [CategorieController::class, 'destroy']);
+});
+Route::group(['middleware'=> ['auth:sanctum']], function () {
+    Route::get('produits', [PrduitController::class, 'index']);
+    Route::get('produits/all', [PrduitController::class, 'getAllProducts']);
+    Route::post('produits', [PrduitController::class, 'store']);
+    Route::post('produits/quantite/{produitId}/{fournisseurId}', [PrduitController::class, 'addQuantite']);
+    Route::get('produits/{column}/{param}', [PrduitController::class, 'show']);
+    Route::put('produits/{id}', [PrduitController::class, 'update']);
+    Route::delete('produits/{id}', [PrduitController::class, 'destroy']);
+});
 
+Route::group(['middleware'=> ['auth:sanctum']], function () {
+    Route::get('packs', [PackController::class, 'index']);
+    Route::post('packs', [PackController::class, 'store']);
+    Route::get('packs/{column}/{param}', [PackController::class, 'show']);
+    Route::delete('packs/{id}', [PackController::class, 'destroy']);
+    Route::put('packs/{id}', [PackController::class, 'update']);
+});
 
+Route::group(['middleware'=> ['auth:sanctum']], function () {
+    Route::get('packProduits', [Produit_PackController::class, 'index']);
+    Route::post('packProduits', [Produit_PackController::class, 'store']);
+    Route::get('packProduits/{column}/{param}', [Produit_PackController::class, 'show']);
+    Route::delete('packProduits/{pack_id}/{produit_id}', [Produit_PackController::class, 'destroy']);
+    Route::put('packProduits/{id}', [Produit_PackController::class, 'update']);
+});
+//Route::get('factures', [FactureController::class, 'index']);
+//Route::post('factures', [FactureController::class, 'store']);
+//Route::get('factures/{column}/{param}', [FactureController::class, 'show']);
+//Route::delete('factures/{id}', [FactureController::class, 'destroy']);
+//Route::put('factures/{id}', [FactureController::class, 'update']);
+
+Route::group(['middleware'=> ['auth:sanctum']], function () {
+    Route::get('orderStatuses', [OrderStatusController::class, 'index']);
+    Route::get('orderStatuses/{id}', [OrderStatusController::class, 'show']);
+    Route::post('orderStatuses', [OrderStatusController::class, 'store']);
+    Route::put('orderStatuses/{id}', [OrderStatusController::class, 'update']);
+    Route::delete('orderStatuses/{id}', [OrderStatusController::class, 'destroy']);
+});
+//Route::get('discounts', [DiscountController::class, 'index']);
+//Route::get('discounts/{id}', [DiscountController::class, 'show']);
+//Route::post('discounts', [DiscountController::class, 'store']);
+//Route::put('discounts/{id}', [DiscountController::class, 'update']);
+//Route::delete('discounts/{id}', [DiscountController::class, 'destroy']);
+
+Route::group(['middleware'=> ['auth:sanctum']], function () {
     Route::get('packsCommandes', [PackageCommandeController::class, 'index']);
     Route::post('packsCommandes', [PackageCommandeController::class, 'store']);
 });
-
-
